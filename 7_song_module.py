42da1fd7e6ee3591376fe125cf9e5fe2cd0e9306
Ankit Kumar <ankitsharmaeway.com>
2023-02-17 23:51:31 +0530

    first commit

---
+++ 7_song_module.py
@@ -0,0 +1,5 @@
+from playsound import playsound
+
+#agar hm double back_slash lagate hain to hi run hoga 
+#agar hm single back_slash lagate hai escape sequence ho jata h
+playsound('C:\\Users\Ankit\\OneDrive\\Documents\\PYTHON\\shaun-frank-bon-appetit-feat-ya-le-ultra-music_ts9s7V4o.mp3')
